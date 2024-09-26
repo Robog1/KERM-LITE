da373156077e0b348b67f4e6c0dac7b7f93fe2c1
Robog1 <testdulite2@outlook.com>
2024-09-26 18:03:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
    AUTO_REACT: toBool(process.env.AUTO_REACT) || false,
 
    // String Values
-   SESSION_ID: process.env.SESSION_ID || "Session~FKF0wD1C", // Put your session
+   SESSION_ID: process.env.SESSION_ID || "Session~07v2hEnP", // Put your session
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[!]",
-   BOT_INFO: process.env.BOT_INFO || "Kɢᴛᴇᴄʜ;KᴇʀᴍLɪᴛᴇ⚡️;https://telegra.ph/file/3ac47cbad32a86aa12251.jpg", // YourName;BotName;Image/VideoLink
+   BOT_INFO: process.env.BOT_INFO || "Kɢᴛᴇᴄʜ;KᴇʀᴍLɪᴛᴇ⚡️;https://i.imgur.com/TEdIUBr.mp4", // YourName;BotName;Image/VideoLink
    SUDO: process.env.SUDO || "237683236218",
    PRESENCE: process.env.PRESENCE || "available", // "unavailable", "available", "composing", "recording", "paused"
    CAPTION: process.env.CAPTION || "© Kᴇʀᴍ ᴍᴅ",
